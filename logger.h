fcf9b6558eb258e4c829624e7172ec0803b047e8
Suraj Chakravarthi <cluster.root@gmail.com>
2016-06-07 16:16:09 -0700

    added header for new data-logger(logger.h) library

---
+++ logger.h
@@ -0,0 +1,33 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <NIDAQmx.h>
+#include <stdio.h>
+#include <windows.h>
+#include <iostream>
+#include <utilities.h>
+
+class logger {
+private:
+  enum {
+    CHAR,
+    INT,
+    FLOAT,
+    DOUBLE
+  } TYPE;
+  typedef class {
+    void *data;
+    listElement *prev, *next;
+  } listElement;
+  int dataCount; //number of data points to log
+  listElement *listHead;
+
+public:
+  logger(); //initialize looger's basic variables, pointers to NULL
+  ~logger(); //destroy all dynamic variables here
+  int addElement(void *, TYPE, char *); //add variable pointed to by parameter to list
+  int removeElement(listElement *);
+  int getList(); //get list of variables to read from config file
+}
+
+#endif
