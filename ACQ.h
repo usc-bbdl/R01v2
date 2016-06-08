0da0001d1f79f78ee4319e3aa3de59e0e998dea2
Suraj Chakravarthi <cluster.root@gmail.com>
2016-06-08 12:37:33 -0700

    completed a basic version of ACQ.h

--- ACQ.h
+++ ACQ.h
@@ -10,13 +10,27 @@
 class ACQ {
 private:
   class ACQdata{
-
+    double loadCellData[7];
+    double encoderData[7];
   };
 public:
+  //these functions are generic and are independent of DAQ drivers
   ACQ(); //start NIDAQmx task for acquisition - encoder and loadcell cards to be attached to this task
   ~ACQ(); //stop and clear task, remove any dynamic variables
-  int getData(void *);//gets data from cards and puts data in class pointed to by the void * parameter
+  int getData(ACQdata *); //gets data from cards and puts data in class pointed to by the parameter
 
+  //----------------------//
+  //DAQ specific functions//
+  //----------------------//
+
+  //NIDAQmx specific functions go here
+  int NIstartTask(); //function starts task by calling NIDAQmx functions
+  int NIstopTask(); //stop task
+  int NIclearTask(); //clear task
+  int NIgetData(ACQdata *); //acquire data from NI cards
+
+  //Acromag AcPC482 specific functions here
+  int AMgetData(ACQdata *); //acquire data from Acromag cards - leave empty for now
 };
 
 #endif
