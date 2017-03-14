374aeadf24829f4fd81f13089d8d4256828ea5a4
Kian <seyed.jalaleddini@mail.mcgill.ca>
2017-03-14 15:51:25 -0700

    declaring constructor and destructor and the two function for reading force in fpgaIONeuromorphic

--- fpgaIONeuromorphic.h
+++ fpgaIONeuromorphic.h
@@ -1,3 +1,19 @@
-muscleFPGA  = new fpgaIONeuromorphic(MOTOR_FPGA, BICEP);
-muscleFPGA->readForceStream(&muscleForceFpgaPipe);
-muscleFPGA->readForceWire(&muscleForceFpgaPipe);
+#ifndef FPGANEUROMORPHIC_H
+#define FPGANEUROMORPHIC_H
+#include <iostream>
+#include "utilities.h"
+//#include <pthread.h>
+#include <malloc.h>
+
+class SomeFpga 
+{
+    public:
+        fpgaIONeuromorphic(int , int);
+        ~fpgaIONeuromorphic();
+        int readForceStream(float *);
+        int readForceWire(float *);
+    private:
+        char serX[50];
+};
+
+#endif
