90c44ebae7c02eaf910d1976197caf0fd4051461
Jason Lin <uscjlin@gmail.com>
2017-04-03 15:10:47 -0700

    extracted data variables from motorControl into motorData. TODO: 1. separate control commands from data variables back into motorControl 2. move getters and setters to motorData, make variables private

--- motorControl.h
+++ motorControl.h
@@ -52,7 +52,7 @@ public:
     logger* mLogger;
     bool newPdgm_Flag;
 
-    motorControl(double,double);
+    motorControl();
     ~motorControl(void);
     bool isEnable, isWindUp, isControlling;
     int motorEnable();
