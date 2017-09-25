0cd1600b8f7f8a60771b2edacfd777e313091ce4
USC <git@valerolab.org>
2017-09-24 23:23:05 -0700

    Suraj - basic system is operational for WIRED demo - gamma parameter and GGAIN fine-tuning still need to be done

--- expParadigmMuscleLengthCalibration.cpp
+++ expParadigmMuscleLengthCalibration.cpp
@@ -22,10 +22,12 @@ int expParadigmMuscleLengthCalibration::startParadigm(motorControl *realTimeCont
   float64 muscleLengthBeforePert[2], muscleLengthAfterPert[2];
   realTimeController->encoderBias[0] = realTimeController->encoderBias[1] = 0;
   realTimeController->encoderGain[0] = realTimeController->encoderGain[1] = 1;
-  ggain = GGAIN;
-  tbias = TBIAS;
-  GGAIN = 0;
-  TBIAS = 1;
+  
+  //ggain = GGAIN;
+  //tbias = TBIAS;
+  //GGAIN = 0;
+  //TBIAS = 1;
+
   //code here
   servo->setVelocity(rampVelocity);
   servo->setPosition(initPos);
@@ -53,8 +55,10 @@ int expParadigmMuscleLengthCalibration::startParadigm(motorControl *realTimeCont
   realTimeController->encoderBias[1] = bias[1];
   realTimeController->encoderGain[0] = gain[0];
   realTimeController->encoderGain[1] = gain[1];
-  GGAIN = ggain;
-  TBIAS = tbias;
+  
+  //GGAIN = ggain;
+  //TBIAS = tbias;
+
   printf("Calibration finished. Press space to continue.\n");
   return 1;
 }
--- utilities.cpp
+++ utilities.cpp
@@ -10,7 +10,7 @@
 #include <FPGAControl.h>
 
 //float GGAIN = 0.0125; //default is (0.9/1000) //0.4/2000 is safe
-float GGAIN = 0.000016923;//0.005; //default is (0.9/1000) //0.4/2000 is safe
+float GGAIN = 0.000045;//0.000016923;//0.005; //default is (0.9/1000) //0.4/2000 is safe
 float TBIAS = 2;
 
 int dataAcquisitionFlag[12] = {1,1,0,0,0,0,0,0,0,0,0,0}; //force(0), EMG(1), spindleIa(2), spindleII(3),spikeCount(4),raster1(5),raster2(6),raster3(7),raster4(8),raster5(9),raster6(10), real-time control cortex(11)
